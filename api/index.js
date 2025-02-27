4331ee98c16b87c844947342e5a03e31300071ea
dithprtm <73995261+dithprtm@users.noreply.github.com>
2025-02-27 11:47:36 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,20 +4,19 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// Mendapatkan path absolute
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// ✅ Pastikan Express menemukan folder public
+
 app.use(express.static(join(__dirname, "../public")));
 
 app.set("views", join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index"); // Harus ada views/index.ejs
+  res.render("index"); 
 });
 
-// Handler untuk Vercel
 export default (req, res) => {
   return app(req, res);
 };
