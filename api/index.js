1ded90593b51f7d0f6510264f5f97b3936f9ff21
dithprtm <73995261+dithprtm@users.noreply.github.com>
2025-02-27 10:57:09 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,12 @@
 import express from "express";
-import { createServer } from "@vercel/node";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.set("view engine", "ejs");
-
-// Atur folder views & public agar bisa digunakan
-app.set("views", path.join(__dirname, "../views"));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-// Tidak pakai app.listen(), karena Vercel hanya mendukung serverless function
-export default createServer(app);
-  
+export default app;
